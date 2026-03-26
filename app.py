3ef4144212c0c5ff52b070db18081dd67f3de157
julcan29-netizen <jul.can29@gmail.com>
2026-03-26 14:04:37 +0100

    app.py

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ st.title("Générateur de schéma simple")
 text = st.text_area("Décris ton installation")
 
 if st.button("Générer"):
-    st.markdown("""
-```mermaid
+    code = """```mermaid
 flowchart TD
 A[Analyse] --> B[Installation]
 B --> C[Pompe]
 B --> D[Vanne]
 B --> E[Ventilation]
+```"""
+    st.markdown(code)
