0eb379effc9fe36772e509291a2eecc003fa46e4
julcan29-netizen <jul.can29@gmail.com>
2026-03-27 17:57:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
+from folio_power_v2 import render_power_folio_streamlit
 
 st.set_page_config(layout="wide")
 
-st.title("APP FINAL TEST")
+st.title("APP FINAL TEST V2")
 
-st.success("SI TU VOIS CE MESSAGE → APP.PY EST BIEN CELUI-CI")
-
-from folio_power import render_power_folio_streamlit
+st.success("SI TU VOIS CE MESSAGE → IMPORT V2 OK")
 
 render_power_folio_streamlit()
