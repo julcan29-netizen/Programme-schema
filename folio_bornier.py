e3bb4f80e857ee5b4ab5d6d748646aeaffc12cde
julcan29-netizen <jul.can29@gmail.com>
2026-03-27 13:03:15 +0100

    Create folio_bornier.py

---
+++ folio_bornier.py
@@ -0,0 +1,8 @@
+def svg_bornier():
+    return """
+<svg width="1200" height="700" xmlns="http://www.w3.org/2000/svg" style="background:#ffffff">
+  <rect x="20" y="20" width="1160" height="660" fill="none" stroke="#222" stroke-width="1.5"/>
+  <text x="40" y="50" font-size="22">Folio 15 - Bornier X1</text>
+  <text x="40" y="78" font-size="12">En cours de verrouillage</text>
+</svg>
+"""
