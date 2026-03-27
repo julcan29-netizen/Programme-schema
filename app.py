b460604392320bc40437705a160546e062517b2c
julcan29-netizen <jul.can29@gmail.com>
2026-03-27 17:45:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,6 @@
-# app.py
 import streamlit as st
-from folio_power import render_power_folio_streamlit
 
 st.set_page_config(layout="wide")
-st.title("Générateur de schémas électriques CVC")
-
-page = st.sidebar.selectbox(
-    "Choisir un folio",
-    ["Folio 10 - Puissance"]
-)
-
-if page == "Folio 10 - Puissance":
-    render_power_folio_streamlit()
+st.title("TEST DEPLOIEMENT")
+st.error("VERSION TEST 999")
+st.write("Si tu vois ce message, alors GitHub/Streamlit déploient bien le bon code.")
