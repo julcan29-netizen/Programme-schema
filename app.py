e6d2d71f6936b6f358db01a971fe0eef0eeb7b20
julcan29-netizen <jul.can29@gmail.com>
2026-03-27 17:55:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import streamlit as st
-from folio_power import render_power_folio_streamlit
 
 st.set_page_config(layout="wide")
-st.title("Générateur de schémas électriques CVC")
+
+st.title("APP FINAL TEST")
+
+st.success("SI TU VOIS CE MESSAGE → APP.PY EST BIEN CELUI-CI")
+
+from folio_power import render_power_folio_streamlit
 
 render_power_folio_streamlit()
