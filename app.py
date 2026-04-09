9bfbf414434c9ebd5648d7c63e301e591c3747d7
julcan29-netizen <jul.can29@gmail.com>
2026-04-09 16:29:25 +0200

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from folio_power_v3 import render_power_folio_streamlit
-
-st.set_page_config(layout="wide")
-st.title("Générateur de schémas électriques CVC")
-
-render_power_folio_streamlit()
